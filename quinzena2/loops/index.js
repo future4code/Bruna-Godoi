fec2986c7b5cc3b8df729f8c5b9409fac3cd9b1e
BrunaGodoi <brunagodoi.dev@gmail.com>
2021-09-28 21:26:16 -0300

    Exercicios de laços

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,13 @@
+/* Exercícios de Interpretação
+----- Exercício 1.
+Não consegui entender o porque imprimiu o 10. Rodei no console, mas não entendi.
+2 - não consegui entender
+3 - não consegui entender
+*/
+
+// Codigo 
+
+const temBichoEstimacao = Number(prompt('Quantos animais de estimação você tem?'))
+
+
+const nomesPets = prompt[("Qual o nome do seu pet?")]
