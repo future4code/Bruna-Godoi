175e94b6ed8923fe4617440626b5adfd656fcc55
BrunaGodoi <brunagodoi.dev@gmail.com>
2021-10-06 20:55:49 -0300

    Adicionado Exercício 6

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -51,7 +51,19 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
-
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3 
+  
+  const respostanoArray = [
+  booleano1 && booleano2 && !booleano4,
+  (booleano1 && booleano2) || !booleano3,
+  (booleano2 || booleano3) && (booleano4 || booleano1),
+  !(booleano2 && booleano3) || !(booleano1 && booleano3),
+  !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+  ]
+  return respostanoArray
 }
 
 // EXERCÍCIO 07
