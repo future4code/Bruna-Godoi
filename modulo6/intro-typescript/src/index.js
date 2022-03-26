9c3f2f63cc62767e6eda77dc58be67c4af9a3e3a
BrunaGodoi <Brunagodoi.dev@gmail.com>
2022-03-25 21:52:08 -0300

    exercicios

---
+++ modulo6/intro-typescript/src/index.js
@@ -0,0 +1,17 @@
+// "terminal.integrated.defaultProfile.windows": "Git Bash",
+// npm i colors
+// ------------------------------Exercicio
+var comparaDoisNumeros = function (num1, num2) {
+    var maiorNumero;
+    var menorNumero;
+    if (num1 > num2) {
+        maiorNumero = num1;
+        menorNumero = num2;
+    }
+    else {
+        maiorNumero = num2;
+        menorNumero = num1;
+    }
+    var diferenca = maiorNumero - menorNumero;
+    return diferenca;
+};
